e464a403e4f560205eb81812034d07b51d37129e
Ercan <78651001+ercandeniz18@users.noreply.github.com>
2023-09-11 12:43:44 +0000

    adding filter funtion

--- map_filter_function.py
+++ map_filter_function.py
@@ -6,5 +6,8 @@ Output the resulting list."""
 
 salaries = [2000, 1800, 3100, 4400, 1500]
 bonus = int(input())
-salaries = list(map(lambda x : x+bonus,salaries))
-print(salaries)
+bonu_salaries = list(map(lambda x : x+bonus,salaries))
+print(bonu_salaries)
+
+smaller_than_3500 = list(filter(lambda x : x<3500,salaries))
+print(smaller_than_3500)
